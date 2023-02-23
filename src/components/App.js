9dddf579d906db33554c4417d4801b5087695a96
Eve-Kamau <evelyne.kamau@student.moringaschool.com>
2023-02-23 16:48:26 +0300

    Completed Lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,31 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+ const [dogPics, setPics] = useState([]);
+ const [isLoading, setIsLoading] = useState(false);
+
+useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random/dogs") 
+        .then((res) => res.json())
+        .then((data) => {
+        setPics(data.message);
+        setIsLoading(true); 
+     });
+
+}, []);
+
+if(!isLoading) {
+return <p>Loading.....</p>;
+} else {
+return (
+    <div>
+    {dogPics.map((dog => (
+    <img src={dog} key={dog} alt="A Random Dog"/> 
+    )))}
+    </div>
+ );
+}
+}
+
+export default App;
